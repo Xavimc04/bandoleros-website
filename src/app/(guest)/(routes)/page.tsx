1372d92ca6dd7b41ff9a612a92ab01eee2a5d520
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-04-10 16:11:49 +0200

    Website resource initicialization

---
+++ src/app/(guest)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <main>
+        Website content
+    </main>
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = "Bandoleros RP"
+export const APP_DESCRIPTION = "Tu experiencia de rol definitiva"
