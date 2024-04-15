9e90dfcda685c9e29dcbe7ffbd9152e7c2617be8
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-04-15 16:16:10 +0200

    Markdown initial content formatter

---
+++ public/devblogs/1-server-installation.md
@@ -0,0 +1,9 @@
+## Instalación del servidor
+
+Se ha generado y planteado la estructura principal del proyecto además de la puesta en marcha del repositorio remoto de Github. 
+
+- Licencias generadas
+- Instalación artifacts
+- TxAdmin instalado y configurado
+
+**Fecha:** _15/04/24_
--- src/components/layouts/landing/markdown.tsx
+++ src/components/layouts/landing/markdown.tsx
@@ -13,7 +13,7 @@ export default async function RenderFileContent({
 
     if(!content) return;
 
-    return <article className="flex flex-col prose prose-pre:bg-zinc-800 text-white prose-img:rounded-md prose-headings:text-red-400">
+    return <article className="flex flex-col prose prose-pre:bg-zinc-800 prose-strong:text-white prose-em:text-orange-400 text-white prose-img:rounded-md prose-headings:text-red-400">
         <Markdown
             remarkPlugins={[remarkGfm]}
         >{ content }</Markdown>
